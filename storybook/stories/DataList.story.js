5dcdeafcd24e7f93a6c430c9968e54095d4a33a2
Felippe Rodrigo Puhle <felippe.puhle@gmail.com>
2017-07-11 17:41:30 -0300

    Add story with render() function

--- storybook/stories/DataList.story.js
+++ storybook/stories/DataList.story.js
@@ -11,7 +11,7 @@ stories.add('default', () =>
   <DataList
     environment={Environment}
     query={graphql`
-      query DataListQuery($first: Int!) {
+      query DataListPeopleQuery($first: Int!) {
         allPeople(first: $first) @connection(key: "DataList_allPeople", filters: []) {
           edges {
             node {
@@ -36,6 +36,48 @@ stories.add('default', () =>
           label: 'Gender',
           property: 'gender',
         },
+      ],
+    }}
+  />,
+);
+
+stories.add('with custom render function', () =>
+  <DataList
+    environment={Environment}
+    query={graphql`
+      query DataListFilmsQuery($first: Int!) {
+        allFilms(first: $first) @connection(key: "DataList_allFilms", filters: []) {
+          edges {
+            node {
+              title
+              director
+              releaseDate
+            }
+          }
+          pageInfo {
+            hasNextPage
+          }
+        }
+      }
+    `}
+    variables={{ first: 5 }}
+    table={{
+      columns: [
+        {
+          label: 'Title',
+          property: 'title',
+        },
+        {
+          label: 'Director',
+          property: 'director',
+        },
+        {
+          label: 'Year',
+          render: (film) => {
+            const releaseDate = new Date(film.releaseDate);
+            return releaseDate.getFullYear();
+          },
+        }
       ],
     }}
   />,
